d03988281007d235427539704c3e33333698b121
Niranjan V <rathinamniranjan@gmail.com>
2026-01-25 16:31:21 +0530

    Home Ui Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./Component/Home";
+import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="flex min-h-screen">
+        <div className="flex-1">Logo with settings</div>
+        <div className="flex-3 border-2 border-purple-600">Dashboard</div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
